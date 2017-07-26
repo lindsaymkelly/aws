943827fc975fea6dcf6ec7be8cdd3e1b4548b506
lindsaymkelly <lindzlady@gmail.com>
2017-07-26 16:39:27 -0400

    Add initial routes with pages controller and view files

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,23 @@
+class PagesController < ApplicationController
+  def index
+  end
+
+  def classes
+  end
+
+  def schedule
+  end
+
+  def results
+  end
+
+  def staff
+  end
+
+  def about
+  end
+
+  def location
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/index' => 'pages#index'
+  get '/classes' => 'pages#classes'
+  get '/schedule' => 'pages#schedule'
+  get '/results' => 'pages#results'
+  get '/staff' => 'pages#staff'
+  get '/about' => 'pages#about'
+  get '/location' => 'pages#location'
+  get '/login' => 'pages#login'
+
+  root 'pages#index'
 end
