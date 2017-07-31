7c03e7644f694fed16eced5278dc41e693e8bf76
lindsaymkelly <lindzlady@gmail.com>
2017-07-31 15:27:00 -0400

    Add CRUD functionality to scheduled classes and results

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -1,16 +1,54 @@
 class ResultsController < ApplicationController
   before_action :authenticate_admin!
+  before_action :find_result, only: [:edit, :update, :destroy]
 
   def index
+    @results = Result.all
+  end
+
+  def new
+    @result = Result.new
   end
 
   def create
+    # debugger
+    @result = Result.new(result_params)
+    if @result.save
+      redirect_to admin_results_path
+    else
+      @errors = @result.errors.full_messages
+      render 'new'
+    end
+  end
+
+  def edit
   end
 
   def update
+    if @result.update(result_params)
+      redirect_to admin_results_path
+    else
+      @errors = @result.errors.full_messages
+      render 'edit'
+    end
   end
 
   def destroy
+    @result.destroy
+
+    redirect_to admin_results_path
   end
+
+  private
+    def result_params
+      result_args = params.require(:result)
+      result_args.merge!(year: Date::strptime(params[:result][:year], "%Y"))
+      result_args.permit(:year, :level, :description, :image)
+    end
+
+    def find_result
+      @result = Result.find(params[:id])
+    end
+
 
 end
--- app/controllers/scheduled_classes_controller.rb
+++ app/controllers/scheduled_classes_controller.rb
@@ -1,26 +1,50 @@
 class ScheduledClassesController < ApplicationController
   before_action :authenticate_admin!
+  before_action :find_scheduled_class, only: [:edit, :update, :destroy]
 
   def index
+    @scheduled_classes = ScheduledClass.all
   end
 
   def new
+    @scheduled_class = ScheduledClass.new
   end
 
   def create
+    @scheduled_class = ScheduledClass.new(scheduled_class_params)
+    if @scheduled_class.save
+      redirect_to admin_schedule_path
+    else
+      @errors = @scheduled_class.errors.full_messages
+      render 'new'
+    end
   end
 
   def edit
   end
 
   def update
+    if @scheduled_class.update(scheduled_class_params)
+      redirect_to admin_schedule_path
+    else
+      @errors = @scheduled_class.errors.full_messages
+      render 'edit'
+    end
   end
 
   def destroy
+    @scheduled_class.destroy
+
+    redirect_to admin_schedule_path
   end
 
   private
     def scheduled_class_params
+      params.require(:scheduled_class).permit(:day, :time, :name, :about, :instructor, :active)
+    end
+
+    def find_scheduled_class
+      @scheduled_class = ScheduledClass.find(params[:id])
     end
 
 end
